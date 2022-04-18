87d60e6979eefaec6a6a7ff16bc1464c844ac9a0
kaminyou ho <kaminyouho@aetherai.com>
2022-04-18 03:44:30 +0000

    consep yaml

--- train_consep.py
+++ train_consep.py
@@ -9,101 +9,51 @@ from models.hovernet import HoVerNetExt
 from process.train import train_step
 from process.utils import proc_valid_step_output
 from process.validate import valid_step
-from tools.utils import update_accumulated_output
+from tools.utils import update_accumulated_output, read_yaml
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Train model with PanNuck dataset")
     parser.add_argument(
-        "--train_data_path",
+        "--config",
+        "-c",
         type=str,
         required=True,
-        help="Path to the config file."
-    )
-    parser.add_argument(
-        "--valid_data_path",
-        type=str,
-        required=True,
-        help="Path to the config file."
-    )
-    parser.add_argument(
-        "--patch_size",
-        type=int,
-        help="Patch size",
-        default=512
-    )
-    parser.add_argument(
-        "--batch_size",
-        type=int,
-        default=2
-    )
-    parser.add_argument(
-        "--epochs",
-        type=int,
-        default=100
-    )
-    parser.add_argument(
-        "--device",
-        type=str,
-        default="cuda"
-    )
-    parser.add_argument(
-        "--num_types",
-        type=int,
-        default=5
-    )
-    parser.add_argument(
-        "--pretrained",
-        type=str,
-        default="./pretrained/resnet50-0676ba61.pth"
-    )
-    parser.add_argument(
-        "--save_step",
-        type=int,
-        default=5,
-        help="Save model for N steps"
-    )
-    parser.add_argument(
-        "--save_path",
-        type=str,
-        default="./experiments/initial/",
-        help="Path to save models"
-    )
-    parser.add_argument(
-        "--verbose",
-        action="store_true"
+        help="yaml config file path"
     )
     args = parser.parse_args()
 
+    config = read_yaml(args.config)
+
     train_dataloader = get_dataloader(
         dataset_type="consep",
-        data_path=args.train_data_path,
+        data_path=config["DATA"]["TRAIN_DATA_PATH"],
         with_type=True,
-        input_shape=(args.patch_size, args.patch_size),
-        mask_shape=(args.patch_size, args.patch_size),
-        batch_size=args.batch_size,
+        input_shape=(config["DATA"]["PATCH_SIZE"], config["DATA"]["PATCH_SIZE"]),
+        mask_shape=(config["DATA"]["PATCH_SIZE"], config["DATA"]["PATCH_SIZE"]),
+        batch_size=config["TRAIN"]["BATCH_SIZE"],
         run_mode="train",
     )
     val_dataloader = get_dataloader(
         dataset_type="consep",
-        data_path=args.valid_data_path,
+        data_path=config["DATA"]["VALID_DATA_PATH"],
         with_type=True,
-        input_shape=(args.patch_size, args.patch_size),
-        mask_shape=(args.patch_size, args.patch_size),
-        batch_size=args.batch_size,
+        input_shape=(config["DATA"]["PATCH_SIZE"], config["DATA"]["PATCH_SIZE"]),
+        mask_shape=(config["DATA"]["PATCH_SIZE"], config["DATA"]["PATCH_SIZE"]),
+        batch_size=config["TRAIN"]["BATCH_SIZE"],
         run_mode="val",
     )
 
     model = HoVerNetExt(
-        pretrained_backbone=args.pretrained,
-        num_types=args.num_types,
+        pretrained_backbone=config["TRAIN"]["PRETRAINED"],
+        num_types=config["DATA"]["NUM_TYPES"],
     )
     optimizer = optim.Adam(model.parameters(), lr=1.0e-4, betas=(0.9, 0.999))
 
-    model.to(args.device)
+    model.to(config["TRAIN"]["DEVICE"])
 
-    os.makedirs(args.save_path, exist_ok=True)
+    os.makedirs(config["LOGGING"]["SAVE_PATH"], exist_ok=True)
 
-    for epoch in range(args.epochs):
+    for epoch in range(config['TRAIN']['EPOCHS']):
         accumulated_output = {}
         for step_idx, data in enumerate(train_dataloader):
             train_result_dict = train_step(
@@ -112,9 +62,9 @@ if __name__ == "__main__":
                 batch_data=data,
                 model=model,
                 optimizer=optimizer,
-                device=args.device,
+                device=config["TRAIN"]["DEVICE"],
                 show_step=1,
-                verbose=args.verbose,
+                verbose=config["LOGGING"]["VERBOSE"],
             )
 
         for step_idx, data in enumerate(val_dataloader):
@@ -123,26 +73,26 @@ if __name__ == "__main__":
                 step_idx,
                 batch_data=data,
                 model=model,
-                device=args.device
+                device=config["TRAIN"]["DEVICE"]
             )
             update_accumulated_output(accumulated_output, valid_result_dict)
 
         out_dict = proc_valid_step_output(accumulated_output)
 
         print(
-            f"[Epoch {epoch + 1} / {args.epochs}] Val || "
+            f"[Epoch {epoch + 1} / {config['TRAIN']['EPOCHS']}] Val || "
             f"ACC={out_dict['scalar']['np_acc']:.3f} || "
             f"DICE={out_dict['scalar']['np_dice']:.3f} || "
             f"MSE={out_dict['scalar']['hv_mse']:.3f}"
         )
 
-        if (epoch + 1) % args.save_step == 0:
+        if (epoch + 1) % config["LOGGING"]["SAVE_STEP"] == 0:
             torch.save(
                 model.state_dict(),
-                os.path.join(args.save_path, f"epoch_{epoch + 1}.pth")
+                os.path.join(config["LOGGING"]["SAVE_PATH"], f"epoch_{epoch + 1}.pth")
             )
 
     torch.save(
         model.state_dict(),
-        os.path.join(args.save_path, "latest.pth")
+        os.path.join(config["LOGGING"]["SAVE_PATH"], "latest.pth")
     )
